b5ebca7eae0a22a7df1ca5bb9ff3dc7726927830
Januar Harianto <januar.harianto@sydney.edu.au>
2023-07-31 02:13:15 +1000

    add draft quickstart file for testing

---
+++ 00_quickstart.R
@@ -0,0 +1,19 @@
+# BIOL2022 Biology Experimental Design and Analysis (BEDA)
+# Quick-start script for students
+#
+# Last updated: Jul 2023
+#
+##########################
+# About
+##########################
+#
+# This script can be used with RStudio/Posit Cloud to quickly set up your 
+# environment for use in BEDA. 
+
+pkgs <- c("tidyverse", "tidymodels", "palmerpenguins", "rmarkdown", "quarto")
+
+# Install:
+install.packages(pkgs)
+
+
+
