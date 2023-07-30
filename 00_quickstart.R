6d548403be6c2196a334a75eb4337f181683364b
Januar Harianto <januar.harianto@sydney.edu.au>
2023-07-31 02:31:37 +1000

    add package here and also add code to create folder for user

--- 00_quickstart.R
+++ 00_quickstart.R
@@ -10,12 +10,14 @@
 # This script can be used with RStudio/Posit Cloud to quickly set up your 
 # environment for use in BEDA. 
 
+# Install necessary packages from CRAN
 pkgs <- c("tidyverse", "tidymodels", "palmerpenguins", "rmarkdown", "quarto", 
-          "shiny", "learnr", "patchwork", "cowplot", "gganimate")
-
-
-# Install:
+          "shiny", "learnr", "patchwork", "cowplot", "gganimate", "here")
 install.packages(pkgs)
 
+
+# Create user directory
+library(here)
+if (!dir.exists(here("user"))) {dir.create(here("user"))}
 
 
