6c797e87c6b4520780e93f1e9ad916e2b99bae18
Januar Harianto <januar.harianto@sydney.edu.au>
2023-08-01 00:45:38 +1000

    clean up code in quickstart

--- quickstart.R
+++ quickstart.R
@@ -7,17 +7,22 @@
 # About
 ##########################
 #
-# This script can be used with RStudio/Posit Cloud to quickly set up your 
-# environment for use in BEDA. 
+# This script can be used with RStudio/Posit Cloud to quickly set up your
+# environment for use in BEDA.
+
+# RUN THIS SCRIPT ONLY ONCE (but you can run it again if you need to, just
+# takes time to reinstall packages that have already been installed).
 
 # Install necessary packages from CRAN
-pkgs <- c("tidyverse", "tidymodels", "palmerpenguins", "rmarkdown", "quarto", 
-          "shiny", "learnr", "patchwork", "cowplot", "gganimate", "here")
+pkgs <- c(
+    "tidyverse", "tidymodels", "palmerpenguins", "rmarkdown", "quarto",
+    "shiny", "learnr", "patchwork", "cowplot", "gganimate", "here"
+)
 install.packages(pkgs)
 
 
 # Create user directory
 library(here)
-if (!dir.exists(here("user"))) {dir.create(here("user"))}
-
-
+if (!dir.exists(here("user"))) {
+    dir.create(here("user"))
+}
